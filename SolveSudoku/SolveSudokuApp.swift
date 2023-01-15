4bdb90cb07d1392e43a34cd6b48cdf704b2839cb
Wiljo de Ruiter <wiljo.de.ruiter@ziggo.nl>
2023-01-15 20:34:44 +0100

    Initial Commit

---
+++ SolveSudoku/SolveSudokuApp.swift
@@ -0,0 +1,17 @@
+//
+//  SolveSudokuApp.swift
+//  SolveSudoku
+//
+//  Created by Wiljo de Ruiter on 2023-01-15.
+//
+
+import SwiftUI
+
+@main
+struct SolveSudokuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
