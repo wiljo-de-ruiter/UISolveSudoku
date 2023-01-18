49908c59b64cb90f7a8ef9e8852cabb4f9d8b9d0
Wiljo de Ruiter <wiljo.de.ruiter@ziggo.nl>
2023-01-18 22:36:37 +0100

    Lock portrait view

--- SolveSudoku/SolveSudokuApp.swift
+++ SolveSudoku/SolveSudokuApp.swift
@@ -8,10 +8,22 @@
 import SwiftUI
 
 @main
-struct SolveSudokuApp: App {
+struct SolveSudokuApp: App
+{
+    @UIApplicationDelegateAdaptor( AppDelegate.self ) var appDelegate
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+    }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate
+{
+    static var orientationLock = UIInterfaceOrientationMask.all //By default you want all your views to rotate freely
+
+    func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask
+    {
+        return AppDelegate.orientationLock
     }
 }
