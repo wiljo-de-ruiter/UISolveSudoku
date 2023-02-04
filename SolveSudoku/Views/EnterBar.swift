5938fbd773c472e5c1092f29d0cb840c99d27d00
Wiljo de Ruiter <wiljo.de.ruiter@ziggo.nl>
2023-02-04 13:55:37 +0100

    Updates enter bar appearance

--- SolveSudoku/Views/EnterBar.swift
+++ SolveSudoku/Views/EnterBar.swift
@@ -17,15 +17,26 @@ struct EnterButton: View {
         }) {
             let selected = enter != 0 && digit == enter
             let backColor = selected ? Color.selectedBackColor : Color.lightLineColor
-            RoundedRectangle( cornerRadius: 7 )
-                .fill( backColor )
-                .aspectRatio(CGFloat(0.7), contentMode: .fit)// contentMode: .fill )
-                .overlay() {
-                    Text( "\(digit)" )
-                        .fontWeight( selected ? .heavy : .light )
-                        .font( .title )
-                        .foregroundColor( Color.normalTextColor )
-                }
+            ZStack {
+                RoundedRectangle( cornerRadius: 7 )
+                    .fill( backColor )
+                    .aspectRatio(CGFloat(0.6), contentMode: .fit)// contentMode: .fill )
+//                    .overlay() {
+//                        Text( "\(digit)" )
+//                            .fontWeight( selected ? .heavy : .light )
+//                            .font( .title )
+//                            .foregroundColor( Color.normalTextColor )
+//                    }
+                RoundedRectangle( cornerRadius: 7 )
+                    .stroke(Color.normalTextColor, lineWidth: 3 )
+                    .aspectRatio(CGFloat(0.6), contentMode: .fit)// contentMode: .fill )
+                    .overlay() {
+                        Text( "\(digit)" )
+                            .fontWeight( selected ? .heavy : .light )
+                            .font( .title )
+                            .foregroundColor( Color.normalTextColor )
+                    }
+            }
         }
     }
 }
@@ -34,13 +45,128 @@ struct EnterBar: View {
     @Binding var enter: Int
     
     var body: some View {
-        HStack( spacing: 2 ) {
+        HStack {
             Spacer()
             ForEach( 1..<10 ) { digit in
                 EnterButton( enter: $enter, digit: digit )
             }
             Spacer()
         }
+
+/*        VStack( spacing: 2 ) {
+            HStack {
+                VStack {
+                    HStack( spacing: 0 ) {
+                        Image(systemName: "a.circle")
+                    }
+                    HStack( spacing: 0 ) {
+                        Image(systemName: "a.circle")
+                        Image(systemName: "a.circle")
+                    }
+                    HStack( spacing: 0 ) {
+                        Image(systemName: "a.circle")
+                        Image(systemName: "a.circle")
+                        Image(systemName: "a.circle")
+                    }
+                    HStack( spacing: 0 ) {
+                        Image(systemName: "a.circle")
+                        Image(systemName: "a.circle")
+                        Image(systemName: "a.circle")
+                        Image(systemName: "a.circle")
+                    }
+                    HStack( spacing: 0 ) {
+                        Image(systemName: "a.circle")
+                        Image(systemName: "a.circle")
+                        Image(systemName: "a.circle")
+                        Image(systemName: "a.circle")
+                        Image(systemName: "a.circle")
+                    }
+                    HStack( spacing: 0 ) {
+                        Image(systemName: "a.circle")
+                        Image(systemName: "a.circle")
+                        Image(systemName: "a.circle")
+                        Image(systemName: "a.circle")
+                        Image(systemName: "a.circle")
+                        Image(systemName: "a.circle")
+                    }
+                }
+                VStack {
+                    HStack( spacing: 0 ) {
+                        Image(systemName: "a.circle")
+                    }
+                    HStack( spacing: 0 ) {
+                        Image(systemName: "a.circle")
+                        Image(systemName: "a.circle")
+                    }
+                    HStack( spacing: 0 ) {
+                        Image(systemName: "a.circle")
+                        Image(systemName: "circle").foregroundColor(.white)
+                        Image(systemName: "a.circle")
+                    }
+                    HStack( spacing: 0 ) {
+                        Image(systemName: "a.circle")
+                        Image(systemName: "circle").foregroundColor(.white)
+                        Image(systemName: "circle").foregroundColor(.white)
+                        Image(systemName: "a.circle")
+                    }
+                    HStack( spacing: 0 ) {
+                        Image(systemName: "a.circle")
+                        Image(systemName: "a.circle")
+                        Image(systemName: "a.circle")
+                        Image(systemName: "a.circle")
+                        Image(systemName: "a.circle")
+                    }
+                }
+                VStack {
+                    HStack( spacing: 0 ) {
+                        Image(systemName: "a.circle")
+                    }
+                    HStack( spacing: 0 ) {
+                        Image(systemName: "a.circle")
+                        Image(systemName: "a.circle")
+                    }
+                    HStack( spacing: 0 ) {
+                        Image(systemName: "a.circle")
+                        Image(systemName: "circle").foregroundColor(.white)
+                        Image(systemName: "a.circle")
+                    }
+                    HStack( spacing: 0 ) {
+                        Image(systemName: "a.circle")
+                        Image(systemName: "a.circle")
+                        Image(systemName: "a.circle")
+                        Image(systemName: "a.circle")
+                    }
+                }
+                VStack {
+                    HStack( spacing: 0 ) {
+                        Image(systemName: "a.circle")
+                    }
+                    HStack( spacing: 0 ) {
+                        Image(systemName: "a.circle")
+                        Image(systemName: "a.circle")
+                    }
+                    HStack( spacing: 0 ) {
+                        Image(systemName: "a.circle")
+                        Image(systemName: "a.circle")
+                        Image(systemName: "a.circle")
+                    }
+                }
+                VStack {
+                    HStack( spacing: 0 ) {
+                        Image(systemName: "a.circle")
+                    }
+                    HStack( spacing: 0 ) {
+                        Image(systemName: "a.circle")
+                        Image(systemName: "a.circle")
+                    }
+                }
+                VStack {
+                    HStack( spacing: 0 ) {
+                        Image(systemName: "a.circle")
+                    }
+                }
+            }
+        }*/
     }
 }
 
